163e34e77621f8f70058983e9bd8751e52d4b7ef
Uddhav <uddhavpisharody03@gmail.com>
2024-06-18 16:30:48 +0530

    Added comment

--- src/engine.py
+++ src/engine.py
@@ -18,6 +18,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
 
         loss_dict = model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
+        # Accumulate loss
 
         optimizer.zero_grad()
         losses.backward()
